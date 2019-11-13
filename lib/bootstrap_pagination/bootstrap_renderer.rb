7bf6491911a8a7edb3d1b90d8babc10e06bda77b
Vicente Balderas <vbm1191@gmail.com>
2019-11-13 16:37:16 -0600

    Remove filterrific params in pagination (FFA)

--- lib/bootstrap_pagination/bootstrap_renderer.rb
+++ lib/bootstrap_pagination/bootstrap_renderer.rb
@@ -61,5 +61,22 @@ module BootstrapPagination
     def ul_class
       ["pagination", @options[:class]].compact.join(" ")
     end
+
+    def url(page)
+      @base_url_params ||= begin
+        url_params = merge_get_params(default_url_params)
+        url_params[:only_path] = true
+        merge_optional_params(url_params)
+      end
+
+      url_params = @base_url_params.dup
+
+      url_params.delete(:filterrific)
+      url_params.delete(:_)
+      url_params.delete(:utf8)
+      add_current_page_param(url_params, page)
+
+      @template.url_for(url_params)
+    end
   end
 end
